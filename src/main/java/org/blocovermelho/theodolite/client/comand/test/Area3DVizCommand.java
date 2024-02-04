9b0c84d793da42ee3272604abddad9f964455080
Alice Isabel <roridev@protonmail.com>
2024-02-04 18:13:00 -0300

    Add a subcommand for visualizing arbirary cuboid zones inside the current Region2I OctNode

--- src/main/java/org/blocovermelho/theodolite/client/comand/test/Area3DVizCommand.java
+++ src/main/java/org/blocovermelho/theodolite/client/comand/test/Area3DVizCommand.java
@@ -14,21 +14,60 @@ import org.blocovermelho.theodolite.core.pos.Region2I;
 import org.blocovermelho.theodolite.core.utils.NumericalConstants;
 import org.blocovermelho.theodolite.core.utils.render.types.Color4f;
 
-import java.util.Objects;
-
+import static dev.xpple.clientarguments.arguments.CBlockPosArgumentType.blockPos;
+import static dev.xpple.clientarguments.arguments.CBlockPosArgumentType.getCBlockPos;
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.argument;
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
 
 public class Area3DVizCommand {
     public static void register(CommandDispatcher<FabricClientCommandSource> dispatcher) {
         dispatcher.register(literal("a3dc")
+                .then(
+                        argument("pos1", blockPos()).then(
+                                argument("pos2", blockPos())
+                                        .executes(ctx -> {
+                                            DebugRenderer.getInstance().unsetBoxes();
+                                            var source = ctx.getSource();
+
+                                            ClientPlayerEntity player = source.getPlayer();
+                                            ChunkPos chunkPos = player.getChunkPos();
+
+                                            Pos3I pos1 = Pos3I.of(getCBlockPos(ctx, "pos1"));
+                                            Pos3I pos2 = Pos3I.of(getCBlockPos(ctx, "pos2"));
+
+                                            Area3I zoneArea = new Area3I(pos1, pos2);
+
+                                            Region2I region = Region2I.of(chunkPos);
+
+                                            OctNode<String> octree = OctNode.of(region);
+
+
+                                            octree.setValue(zoneArea, "value");
+
+                                            player.sendMessage(Text.of("Octree: " + octree));
+                                            player.sendMessage(Text.of("Zone Area:" + zoneArea));
+
+                                            var iter = new OctTreeNodeIterator<>(octree, false);
+
+                                            DebugRenderer.getInstance().addBox(zoneArea, new Color4f(0.f, 1.f, 0.f,  1.f));
+
+                                            iter.forEachRemaining(x -> {
+                                                if (x.value != null) {
+                                                    DebugRenderer.getInstance().addBox(x.sectionPos, new Color4f(0.f, 0.f,  1.f, 1.0f));
+                                                } else {
+                                                    DebugRenderer.getInstance().addBox(x.sectionPos, new Color4f(1.f, 0.f,  0.f, 0.1f));
+                                                }
+
+                                            });
+
+                                            return 1;
+                                        })
+                        ))
                 .executes(
                     c -> {
-
-
                         DebugRenderer.getInstance().unsetBoxes();
 
                         var source = c.getSource();
-                        var pos =  Pos3D.of(Objects.requireNonNull(source.getClient().getCameraEntity()).getPos());
 
                         ClientPlayerEntity player = source.getPlayer();
                         ChunkPos chunkPos = player.getChunkPos();
